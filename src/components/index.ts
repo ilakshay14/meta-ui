36a0c8e1cd5c4c8352348dcb65e6153dc74df118
Lakshay Malhotra <malhotra.lakshay93@gmail.com>
2022-04-24 14:19:16 +0530

    fixed exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
 export { default as Button } from "./Button";
+export { default as Card } from "./Card";
+export { default as Input } from "./Input";
+export { default as Select } from "./Select";
+export { default as Avatar } from "./Avatar";
