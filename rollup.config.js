f3a4d41823a7021808abcda19e4b47960d547cf6
Lakshay Malhotra <malhotra.lakshay93@gmail.com>
2022-04-24 16:29:31 +0530

    fixed styled component dependency issue

--- rollup.config.js
+++ rollup.config.js
@@ -25,7 +25,9 @@ export default [
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
         ],
+        external: ["styled-components"],
     },
+
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
