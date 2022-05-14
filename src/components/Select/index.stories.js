9f1d1fdea0597d8eacd31237ce6d167aaf90618d
Lakshay Malhotra <malhotra.lakshay93@gmail.com>
2022-05-15 01:34:57 +0530

    storyshot working

--- src/components/Select/index.stories.js
+++ src/components/Select/index.stories.js
@@ -1,35 +1,13 @@
-import React from "react";
-
+// import React from "react";
 import SelectBox from ".";
+import { commonArgs } from './index.util';
 
-const commonArgs = {
-    options: [
-        {
-            id: 1,
-            name: "test 1",
-            value: "test 1",
-        },
-        {
-            id: 2,
-            name: "test 2",
-            value: "test 2",
-        },
-    ],
-    name: "",
-    width: "200px",
-    height: "30px",
-    border: "2px solid #b5b5b5",
-    borderRadius: "3px",
-    padding: "5px",
-    disabled: false,
-    onChange: () => { },
-};
 
 export default {
     component: SelectBox,
 };
 
-const Template = (args) => (
+const Template = (args) =>
     <SelectBox {...args}>
         {args.options.length ? (
             args.options.map((item) => (
@@ -41,7 +19,6 @@ const Template = (args) => (
             <option value=''>No Options provided</option>
         )}
     </SelectBox>
-);
 
 export const Default = Template.bind({});
 Default.args = {
